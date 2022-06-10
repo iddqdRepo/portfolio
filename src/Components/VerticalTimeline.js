8359657ab05fb2c26af2abb140fc88960a58aa65
iddqd <chris.walker.beng@gmail.com>
2022-06-10 11:33:55 +0100

    Altered vertical timeline date

--- src/Components/VerticalTimeline.js
+++ src/Components/VerticalTimeline.js
@@ -1,4 +1,7 @@
-import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 // import "../styles.css";
 import React from "react";
@@ -13,16 +16,23 @@ const Timeline = () => {
             className="vertical-timeline-element--work"
             //   contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
             contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-            date="2019 - present"
+            date="2020 - present"
             iconStyle={{ background: "#006f7b", color: "#fff" }}
             // icon={<span class="iconify" data-icon="ion:school-outline" />}
             icon={<span class="iconify" data-icon="bi:code" />}
           >
-            <span class="main-badge mr-2 mb-2 badge badge-pill">MongoDB, Express, React, NodeJS</span>
-            <h3 className="vertical-timeline-element-title">Full MERN Stack Developer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Northern Ireland</h4>
+            <span class="main-badge mr-2 mb-2 badge badge-pill">
+              MongoDB, Express, React, NodeJS
+            </span>
+            <h3 className="vertical-timeline-element-title">
+              Full MERN Stack Developer
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Northern Ireland
+            </h4>
             <p>
-              Javascript developer foocusing on full stack websites built from scratch:
+              Javascript developer foocusing on full stack websites built from
+              scratch:
               <br /> - BrightEyes Animal Sanctuary
               <br /> - 1v1 Sudoku
               <br /> - Internal Monitoring Tools
@@ -32,16 +42,21 @@ const Timeline = () => {
             className="vertical-timeline-element--work"
             //   contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
             contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-            date="2018 - Late 2019"
+            date="2019 - 2020"
             iconStyle={{ background: "#006f7b", color: "#fff" }}
             // icon={<span class="iconify" data-icon="ion:school-outline" />}
             icon={<span class="iconify" data-icon="bi:code" />}
           >
             <span class="main-badge mr-2 mb-2 badge badge-pill">Python 3</span>
-            <h3 className="vertical-timeline-element-title">Freelance Python Developer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Northern Ireland</h4>
+            <h3 className="vertical-timeline-element-title">
+              Freelance Python Developer
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Northern Ireland
+            </h4>
             <p>
-              Python developer working on internal projects, unfortunately NDA's were signed so my details are sparse. Projects included:
+              Python developer working on internal projects, unfortunately NDA's
+              were signed so my details are sparse. Projects included:
               <br /> - Client file system scrapers
               <br /> - Data Quality analysis
               <br /> - Internal Monitoring Tools
@@ -49,15 +64,21 @@ const Timeline = () => {
           </VerticalTimelineElement>
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2017 - 2019"
+            date="2017 - 2029"
             iconStyle={{ background: "#006f7b", color: "#fff" }}
             icon={<span class="iconify" data-icon="bi:code" />}
           >
-            <h3 className="vertical-timeline-element-title">Managing Director</h3>
-            <h4 className="vertical-timeline-element-subtitle">Northern Ireland</h4>
+            <h3 className="vertical-timeline-element-title">
+              Managing Director
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Northern Ireland
+            </h4>
             <p>
-              Created an importing and eCommerce company who's brands had a yearly turnover of over £500,000. It didn't satisfy my passion for
-              programming, so I sold the brands and became a freelance developer.
+              Created an importing and eCommerce company who's brands had a
+              yearly turnover of over £500,000. It didn't satisfy my passion for
+              programming, so I sold the brands and became a freelance
+              developer.
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
@@ -66,12 +87,20 @@ const Timeline = () => {
             iconStyle={{ background: "#006f7b", color: "#fff" }}
             icon={<span class="iconify" data-icon="bi:code" />}
           >
-            <span class="main-badge mr-2 mb-2 badge badge-pill">Regex, VBA, SQL</span>
-            <h3 className="vertical-timeline-element-title">Data Quality Analyst</h3>
-            <h4 className="vertical-timeline-element-subtitle">Allstate, Northern Ireland</h4>
+            <span class="main-badge mr-2 mb-2 badge badge-pill">
+              Regex, VBA, SQL
+            </span>
+            <h3 className="vertical-timeline-element-title">
+              Data Quality Analyst
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Allstate, Northern Ireland
+            </h4>
             <p>
-              <br /> - Achieved Informatica IDQ Level 1 and Informatica Advanced Techniques
-              <br /> - Trained new team members on Informatica Data Quality client
+              <br /> - Achieved Informatica IDQ Level 1 and Informatica Advanced
+              Techniques
+              <br /> - Trained new team members on Informatica Data Quality
+              client
               <br /> - Created Job aids for Informatica Data Quality client
               <br /> - Regular expressions proficiency
               <br /> - SQL queries for data validation
@@ -87,15 +116,25 @@ const Timeline = () => {
             iconStyle={{ background: "#006f7b", color: "#fff" }}
             icon={<span class="iconify" data-icon="bi:code" />}
           >
-            <span class="main-badge mr-2 mb-2 badge badge-pill">ServiceNow, VBA, HPSM</span>
+            <span class="main-badge mr-2 mb-2 badge badge-pill">
+              ServiceNow, VBA, HPSM
+            </span>
             <h3 className="vertical-timeline-element-title">Change Analyst</h3>
-            <h4 className="vertical-timeline-element-subtitle">Allstate, Northern Ireland</h4>
+            <h4 className="vertical-timeline-element-subtitle">
+              Allstate, Northern Ireland
+            </h4>
             <p>
-              <br /> - Facilitated meetings and produced agenda for CAB (Change Advisory Board)
-              <br /> - Maintained SQL and VBA for Change and Release Management Microsoft Access Utility
-              <br /> - Created user guides and documentation for change and release management process during the migration from HPSM to ServiceNow
-              <br /> - Trained users being onboard to Change and Release records on ServiceNow
-              <br /> - Facilitated change management changes during change freezes
+              <br /> - Facilitated meetings and produced agenda for CAB (Change
+              Advisory Board)
+              <br /> - Maintained SQL and VBA for Change and Release Management
+              Microsoft Access Utility
+              <br /> - Created user guides and documentation for change and
+              release management process during the migration from HPSM to
+              ServiceNow
+              <br /> - Trained users being onboard to Change and Release records
+              on ServiceNow
+              <br /> - Facilitated change management changes during change
+              freezes
             </p>
           </VerticalTimelineElement>
           <VerticalTimelineElement
@@ -104,9 +143,15 @@ const Timeline = () => {
             iconStyle={{ background: "#00a287", color: "#fff" }}
             icon={<span class="iconify" data-icon="ion:school-outline"></span>}
           >
-            <span class="main-badge mr-2 mb-2 badge badge-pill">High 2:1 grade</span>
-            <h3 className="vertical-timeline-element-title">Bachelors Degree in Software Engineering</h3>
-            <h4 className="vertical-timeline-element-subtitle">University of Ulster, Northern Ireland</h4>
+            <span class="main-badge mr-2 mb-2 badge badge-pill">
+              High 2:1 grade
+            </span>
+            <h3 className="vertical-timeline-element-title">
+              Bachelors Degree in Software Engineering
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              University of Ulster, Northern Ireland
+            </h4>
             {/* <p>Strategy, Social Media</p> */}
           </VerticalTimelineElement>
           <VerticalTimelineElement
@@ -115,8 +160,12 @@ const Timeline = () => {
             iconStyle={{ background: "#00a287", color: "#fff" }}
             icon={<span class="iconify" data-icon="ion:school-outline"></span>}
           >
-            <h3 className="vertical-timeline-element-title">Foundation Degree in Science and Computing</h3>
-            <h4 className="vertical-timeline-element-subtitle">Southwest College, Northern Ireland</h4>
+            <h3 className="vertical-timeline-element-title">
+              Foundation Degree in Science and Computing
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Southwest College, Northern Ireland
+            </h4>
             {/* <p>Creative Direction, User Experience, Visual Design</p> */}
           </VerticalTimelineElement>
           <VerticalTimelineElement
@@ -125,8 +174,12 @@ const Timeline = () => {
             iconStyle={{ background: "#00a287", color: "#fff" }}
             icon={<span class="iconify" data-icon="ion:school-outline"></span>}
           >
-            <h3 className="vertical-timeline-element-title">BTEC National Diploma in Information Communication Technologies</h3>
-            <h4 className="vertical-timeline-element-subtitle">Southwest College, Northern Ireland</h4>
+            <h3 className="vertical-timeline-element-title">
+              BTEC National Diploma in Information Communication Technologies
+            </h3>
+            <h4 className="vertical-timeline-element-subtitle">
+              Southwest College, Northern Ireland
+            </h4>
             {/* <p>Creative Direction, Visual Design</p> */}
           </VerticalTimelineElement>
           <VerticalTimelineElement
